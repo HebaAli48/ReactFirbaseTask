0d7e744c1fc459c00744d347a97183bd3cb31ba1
HebaAli48 <131808003+HebaAli48@users.noreply.github.com>
2023-11-04 22:25:50 +0200

    edited in drawer hight

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
   return (
     <nav
       className={`bg-blue-600 text-[white] border-b border-blue-600    ${
-        isCollapsed ? "pb-3" : "  h-[70vh]"
+        isCollapsed ? "pb-3" : "  h-[50vh]"
       } pt-2 sm:pb-2 px-5 sm:px-10`}
     >
       <div className="flex justify-between sm:justify-normal items-center  relative">
@@ -51,7 +51,7 @@ const Header = () => {
         {/* collapse button  */}
         <button
           onClick={() => setIsCollapsed((prev) => !prev)}
-          className="block items-center px-3 py-2 border rounded text-cyan-200 border-blue-400 hover:border-slate-300 hover:text-white hover:border-white ms-2 sm:hidden order-9"
+          className="block items-center px-3 py-2 border rounded text-cyan-200 border-blue-400 hover:border-slate-300 hover:text-white  ms-2 sm:hidden order-9"
         >
           {Menu}
         </button>
@@ -60,9 +60,9 @@ const Header = () => {
         <div
           className={`${
             isCollapsed ? "hidden " : "  "
-          } absolute w-100 top-5 sm:block sm:relative py-10 pl-2 pr-2 sm:text-center sm:top-0 grow bg-inherit  `}
+          } absolute w-100  top-5 sm:block sm:relative py-10 pl-2 pr-2 sm:text-center sm:top-0 grow bg-inherit  `}
         >
-          <ul className="flex flex-col sm:flex-row justify-start sm:justify-center  gap-24 sm:gap-6  items-center w-4/5 m-auto text-lg">
+          <ul className="flex flex-col sm:flex-row justify-start sm:justify-center  gap-14 sm:gap-6  items-center w-4/5 m-auto text-lg">
             {headerData.map((data, index) => (
               <li className="pt-2 sm:pt-0 " key={index}>
                 <NavLink
